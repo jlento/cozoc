e7b760eba204c9c4baf58ee5e6afa7b15335a861
Rantanen Mika P <mika.p.rantanen@helsinki.fi>
2017-12-08 10:59:57 +0200

    F means coriolis parameter, not friction

--- src/io.h
+++ src/io.h
@@ -9,7 +9,7 @@
 enum dimensions { TIME, ZDIM, YDIM, XDIM, NDIMS };
 extern const char* dimnames[NDIMS];
 
-enum fields { TIME_COORDINATE, Z_COORDINATE, FRICTION, NFIELDS };
+enum fields { TIME_COORDINATE, Z_COORDINATE, CORIOLIS, NFIELDS };
 extern const char* fieldnames[NFIELDS];
 
 
