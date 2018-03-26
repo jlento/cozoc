486ab640992d9dad2ea60429c26f303390ef9e2a
Juha Lento <juha.lento@csc.fi>
2018-03-26 08:58:43 +0000

    Minor fix and update

--- env.sh
+++ env.sh
@@ -3,5 +3,5 @@
 thisdir=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
 export PATH=$thisdir/miniconda3/bin:$PATH
-export PKG_CONFIG_PATH=/home/vagrant/tmp/cozoc/miniconda3/lib/pkgconfig:$PKG_CONFIG_PATH
+export PKG_CONFIG_PATH=$thisdir/miniconda3/lib/pkgconfig:$PKG_CONFIG_PATH
 alias emacs="HOME=$thisdir emacs"
